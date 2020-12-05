de740784d3ff3a1cf7cc109365c6fb32a050b438
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-12-05 20:47:59 +0200

    Cards now have basic outlook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import * as React from "react";
 import "./index.css";
 import { Card } from "./Card";
+import styled from "styled-components";
 
 export default () => (
     <>
+    <Cards>
       <Card suit="spade" number="10"/>
       <Card suit="heart" number="5"/>
       <Card suit="diamond" number="3"/>
       <Card suit="club" number="7"/>
+      </Cards>
     </>
   );
+
+const Cards = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: 10vw;
+  margin-top: 10vh;
+  flex-wrap: wrap;
+`;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const suits = {
-    "diamond": "♦",
-    "spade": "♠",
-    "heart": "♥",
-    "club": "♣"
-}
-
+  diamond: "♦",
+  spade: "♠",
+  heart: "♥",
+  club: "♣",
+};
 
 export function Card(props) {
-
-    return (
-        <CardFace>
-            {suits[props.suit]}
-        </CardFace>
-    );
+  return (
+    <CardArea suit={props.suit}>
+      <TopNumber>
+        <CardNumber>{props.number}</CardNumber>
+        <SuitIcon>{suits[props.suit]}</SuitIcon>
+      </TopNumber>
+      <MidSuitIcon>{suits[props.suit]}</MidSuitIcon>
+      <BottomNumber>
+        <CardNumber>{props.number}</CardNumber>
+        <SuitIcon>{suits[props.suit]}</SuitIcon>
+      </BottomNumber>
+    </CardArea>
+  );
 }
 
-const CardFace = styled.div`
-    margin: 0 auto;
-    margin-top: 30vh;
-    height: 320px;
-    width: 200px;
-    background-color: white;
-    border-radius: 25px;
+const MidSuitIcon = styled.div`
+  font-size: 5em;
+  margin: 0 auto;
+`;
+
+const SuitIcon = styled.div`
+  font-size: ${(props) => (props.large ? "4em" : "1.5em")};
+`;
+
+const CardNumber = styled.div`
+  font-size: ${(props) => (props.large ? "4em" : "2em")};
+`;
+
+const TopNumber = styled.div`
+
+`;
+
+const BottomNumber = styled.div`
+    text-align: right;
+`;
+
+const CardArea = styled.div`
+  display: flex;
+  padding: 0.5em;
+  flex-direction: column;
+  justify-content: space-between;
+  color: ${(props) =>
+    props.suit == "heart" || props.suit == "diamond" ? "red" : "black"};
+  height: 320px;
+  width: 200px;
+  background-color: white;
+  border-radius: 25px;
 `;
