42ef619f01e6b3258684843b3430f1429e590a74
JPaivFiare <juho.paivarinta@fiare.com>
2020-12-08 14:32:51 +0200

    Rename file

--- backend/card/card_deal_hands.go
+++ backend/card/card_deal_hands.go
@@ -2,7 +2,6 @@ package card
 
 // DealCardsFromCardDeck deal a 5 card hand for a determined number of players
 func DealCardsFromCardDeck(cardDeck Deck) (dealtCardHands []Deck) {
-
 	for index := range cardDeck {
 		if index%5 == 0 && index != 0 {
 			singleHand := chunkCardDeck(cardDeck, index)
